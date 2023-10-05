3c3edc418c8d3058345e563eb41c340cd475a1d4
techar <35955408+techar@users.noreply.github.com>
2023-10-05 13:20:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     dependency_links=dependency_links,
     extras_require={
         "flash-attn": [
-            "flash-attn>=2.2.1",
+            "flash-attn==1.0.9",
         ],
         "deepspeed": [
             "deepspeed",
